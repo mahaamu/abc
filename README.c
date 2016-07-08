2b9d840baac65b9ba47d69a1a65c4affa1a68f3d
mahaamu <mahalakshmi3901@gmail.com>
2016-07-08 12:06:39 +0530

    Update and rename README.md to README.c
    finish

---
+++ README.c
@@ -0,0 +1,20 @@
+# abc
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int m,n,o;
+printf("enter the no's");
+scanf("%d%d%d",&m&n&o);
+if(m>n)
+{
+prinf("m is greater");
+else
+if(n>o)
+{
+printf("n is greater");
+else
+printf("o is greater");
+}
+}
+}
